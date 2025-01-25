d04488b7c7b50c62e5b7747885b0012e633c6ef8
Ming Xu <mingx@Mings-MacBook-Pro.local>
2025-01-25 13:44:02 -0800

    add moe log

--- java-app/src/main/java/com/example/demo/controller/TestController.java
+++ java-app/src/main/java/com/example/demo/controller/TestController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controller;
 
+import com.example.demo.DemoApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @RestController
 public class TestController {
 
+
+    Logger logger = Logger.getLogger(TestController.class.getName());
+
     @GetMapping("/test")
     public String test() {
+        logger.info("test controller");
         return "Hello from Test Controller!";
     }
 }
