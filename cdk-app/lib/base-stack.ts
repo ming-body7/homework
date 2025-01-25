e21f112178e3ce234a6b76f8acc9bd883b3663a5
Ming Xu <mingx@Mings-MacBook-Pro.local>
2025-01-24 17:24:31 -0800

    persist current status

--- cdk-app/lib/base-stack.ts
+++ cdk-app/lib/base-stack.ts
@@ -13,66 +13,76 @@ export class BaseStack extends cdk.Stack {
     super(scope, id, props);
 
     // Create SQS Queue
-    const queue = new sqs.Queue(this, 'MessageQueue', {
-      queueName: `${props.stage}-demo-message-queue`,
-      visibilityTimeout: cdk.Duration.seconds(300),
+    // const queue = new sqs.Queue(this, 'MessageQueue', {
+    //   queueName: `${props.stage}-demo-message-queue`,
+    //   visibilityTimeout: cdk.Duration.seconds(300),
+    // });
+
+    // Create VPC for EKS
+    const vpc = new cdk.aws_ec2.Vpc(this, `${props.stage}EksVpc`, {
+      maxAzs: 2,
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Public',
+          subnetType: cdk.aws_ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'Private',
+          subnetType: cdk.aws_ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+      ],
     });
 
     // Create EKS Cluster
     const cluster = new eks.Cluster(this, `${props.stage}Cluster`, {
       version: eks.KubernetesVersion.V1_27,
-      defaultCapacity: 2,
+      vpc: vpc,
+      vpcSubnets: [{ subnetType: cdk.aws_ec2.SubnetType.PRIVATE_WITH_EGRESS }],
+      defaultCapacity: 1,
       defaultCapacityInstance: cdk.aws_ec2.InstanceType.of(
         cdk.aws_ec2.InstanceClass.T3,
         cdk.aws_ec2.InstanceSize.MICRO
       ),
     });
 
-    // EKS deployment (Kubernetes manifest) for Spring Boot app
-    // const appDeployment = cluster.addHelmChart('springboot-app', {
-    //   chart: 'my-springboot-app',
-    //   namespace: 'default',
-    //   values: {
-    //     stage: props.stage, // 'beta' or 'prod' pass to k8s
-    //     image: 'my-springboot-app:latest',
-    //     replicas: 3,
-    //   },
-    // });
 
     // Create service account for pod identity
-    const serviceAccount = cluster.addServiceAccount('app-service-account', {
-      name: 'app-service-account',
-      namespace: 'default'
-    });
+    // const serviceAccount = cluster.addServiceAccount('app-service-account', {
+    //   name: 'app-service-account',
+    //   namespace: 'default'
+    // });
 
     // Grant SQS permissions to the service account
-    queue.grantConsumeMessages(serviceAccount);
+    // queue.grantConsumeMessages(serviceAccount);
 
     // Add deployment manifest
-    cluster.addManifest('app-deployment', {
-      apiVersion: 'apps/v1',
-      kind: 'Deployment',
-      metadata: { name: 'spring-app', namespace: 'default' },
-      spec: {
-        replicas: 2,
-        selector: { matchLabels: { app: 'spring-app' } },
-        template: {
-          metadata: { labels: { app: 'spring-app' } },
-          spec: {
-            serviceAccountName: 'app-service-account',
-            containers: [{
-              name: 'spring-app',
-              image: `${props.env?.account}.dkr.ecr.${props.env?.region}.amazonaws.com/spring-app:latest`,
-              ports: [{ containerPort: 8080 }],
-              env: [
-                { name: 'CLOUD_AWS_REGION_STATIC', value: props.env?.region },
-                { name: 'CLOUD_AWS_SQS_QUEUE_NAME', value: queue.queueName }
-              ]
-            }]
-          }
-        }
-      }
-    });
+    // cluster.addManifest('app-deployment', {
+    //   apiVersion: 'apps/v1',
+    //   kind: 'Deployment',
+    //   metadata: { name: 'spring-app', namespace: 'default' },
+    //   spec: {
+    //     replicas: 2,
+    //     selector: { matchLabels: { app: 'spring-app' } },
+    //     template: {
+    //       metadata: { labels: { app: 'spring-app' } },
+    //       spec: {
+    //         serviceAccountName: 'app-service-account',
+    //         containers: [{
+    //           name: 'spring-app',
+    //           image: `${props.env?.account}.dkr.ecr.${props.env?.region}.amazonaws.com/spring-app:latest`,
+    //           ports: [{ containerPort: 8080 }],
+    //           env: [
+    //             { name: 'CLOUD_AWS_REGION_STATIC', value: props.env?.region },
+    //             { name: 'CLOUD_AWS_SQS_QUEUE_NAME', value: queue.queueName }
+    //           ]
+    //         }]
+    //       }
+    //     }
+    //   }
+    // });
 
 
     const repository = new ecr.Repository(this, 'MyRepository', {
